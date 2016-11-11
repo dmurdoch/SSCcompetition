a292bdded19ea4e2c5821d64a8407777371055ea
Duncan Murdoch <murdoch.duncan@gmail.com>
2016-11-11 11:20:45 -0500

    Nov 11 changes

--- R/newStudent.R
+++ R/newStudent.R
@@ -2,6 +2,6 @@ newStudent <- function(name, affiliation = "", date = Sys.Date()) {
   df <- data.frame(name = name, affiliation = affiliation,
              date = date)
   conn <- getConn()
-  RSQLite::dbWriteTable(conn, "Students", value = df, append =TRUE)
+  dbWriteTable(conn, "Students", value = df, append =TRUE)
   doneWith(conn)
 }
