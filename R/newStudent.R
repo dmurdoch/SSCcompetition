68d47089e79ecb58b78da93482183c0ac7a56c41
Duncan Murdoch <murdoch.duncan@gmail.com>
2016-11-14 10:45:03 -0500

    Explicitly convert dates to numeric

--- R/newStudent.R
+++ R/newStudent.R
@@ -1,6 +1,6 @@
 newStudent <- function(name, affiliation = "", date = Sys.Date()) {
   df <- data.frame(name = name, affiliation = affiliation,
-             date = date)
+                   date = as.numeric(date))
   conn <- getConn()
   dbWriteTable(conn, "Students", value = df, append =TRUE)
   doneWith(conn)
--- R/showTable.R
+++ R/showTable.R
@@ -1,5 +1,9 @@
+
 showTable <- function(name) {
   conn <- getConn()
   on.exit(doneWith(conn))
-  dbGetQuery(conn, paste("SELECT * FROM ", name))
+  result <- dbGetQuery(conn, paste("SELECT * FROM ", name))
+  if (is.numeric(result$date))  # FIXME: find a better test!
+    result$date <- as.Date(result$date, origin = "1970-01-01")
+  result
 }
