4aec96c4dcab0eda669296bd246b55652d670cd4
Duncan Murdoch <murdoch.duncan@gmail.com>
2017-02-19 06:03:02 -0500

    Give up on confirmStudent; don't need it.  Other fixes and cleanup

--- R/confirm.R
+++ R/confirm.R
@@ -15,17 +15,3 @@ showName <- function(name, ignore.case = TRUE, exact = FALSE) {
   query <- stringQuery("name", ignore.case, exact)
   showTable("Students", where = query, params = name)
 }
-
-setConfirmed <- function(name, confirmed = TRUE, ignore.case = TRUE, exact = FALSE) {
-  prev <- showName(name, ignore.case = ignore.case, exact = exact)
-  count <- nrow(prev)
-  if (!count)
-    stop("No matching student.")
-  if (count > 1)
-    stop(count, " matching students.")
-  conn <- getConn()
-  on.exit(doneWith(conn))
-  query <- stringQuery("name", name)
-  dbGetQuery(conn, paste("update Students set confirmed =", as.numeric(confirmed),"where", query), params=name)
-  invisible(as.logical(prev$confirmed))
-}
